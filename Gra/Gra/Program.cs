0759ae592377d3c4428bc41139d0dca4dfb53caf
Miikand <mikoand@wp.pl>
2024-11-12 08:35:00 +0100

    add comments to program.cs

--- Gra/Gra/Program.cs
+++ Gra/Gra/Program.cs
@@ -3,12 +3,12 @@ namespace Gra
 {
     class Postac
     {
-        public string nazwa { get; set; }
-        public string Klasa_postaci { get; set; }
-        public int Poziom { get; set; }
-        public int HP { get; set; }
-        public int obrazenia { get; set; }
-        public int doswiadczenie { get; set; } = 0;
+        public string nazwa { get; set; } //nazwa gracza
+        public string Klasa_postaci { get; set; } // klasa postaci na przyklad jakis mag czy cos(trzeba wymyslic)
+        public int Poziom { get; set; } // poziom doswiadczenia lvl
+        public int HP { get; set; } // ilosc zdrowia jaka mamu
+        public int obrazenia { get; set; } // ile zadajemy obrazen
+        public int doswiadczenie { get; set; } = 0; // ile mamy doswiadczenia dana ilosc potrzebna do wbicia poziomu z kazdym poziomem bedzie sie zwieszkac wymagana ilosc
 
 
     }
