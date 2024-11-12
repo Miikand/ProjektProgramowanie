c687622bd775cb290a3b21c304a8d309b99a430b
Miikand <mikoand@wp.pl>
2024-11-12 08:31:05 +0100

    add class postac

--- Gra/Gra/Program.cs
+++ Gra/Gra/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace Gra
+﻿
+namespace Gra
 {
-    internal class Program
+    class Postac
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        public string nazwa { get; set; }
+        public string Klasa_postaci { get; set; }
+        public int Poziom { get; set; }
+        public int HP { get; set; }
+        public int obrazenia { get; set; }
+        public int doswiadczenie { get; set; } = 0;
+
+
     }
 }
--- Gra/Gra/obj/Debug/net8.0/Gra.AssemblyInfo.cs
+++ Gra/Gra/obj/Debug/net8.0/Gra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c54b95bf12b7b3dd46b3b915a93f6f9f399ea57")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
